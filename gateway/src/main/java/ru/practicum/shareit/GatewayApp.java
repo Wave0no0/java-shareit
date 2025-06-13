c7b64c3da381319aad1d57b524c127efe6b1afef
wave <vlad3056968@yandex.ru>
2025-06-14 00:21:32 +0300

    optimisation

--- gateway/src/main/java/ru/practicum/shareit/GatewayApp.java
+++ gateway/src/main/java/ru/practicum/shareit/GatewayApp.java
@@ -1,21 +1,12 @@
 package ru.practicum.shareit;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GatewayApp {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 
 }
